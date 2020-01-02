c9cefd0c663070412a369fcc9dd7ce9a4c795e88
John Stewart <johncodeinaire@gmail.com>
2020-01-02 13:36:25 +1100

    Update - seeds and factories and graphql types (#16)

--- src/db/factories/Recipe.factory.ts
+++ src/db/factories/Recipe.factory.ts
@@ -3,20 +3,32 @@ import * as Faker from 'faker'
 
 import Recipe from '../entities/Recipe'
 import { DifficultyEnum, CostEnum, MealTypeEnum } from '../entities/Recipe'
-import RecipeAttribution from '../entities/RecipeAttribution'
 
-define(Recipe, (
-  faker: typeof Faker,
-  settings: {
-    mealType: MealTypeEnum
-    difficulty: DifficultyEnum
-    cost: CostEnum
-    recipeAttribution: RecipeAttribution
-  }
-) => {
+define(Recipe, (faker: typeof Faker) => {
+  const mealTypeArr = [
+    MealTypeEnum.Breakfast,
+    MealTypeEnum.Lunch,
+    MealTypeEnum.Snack,
+    MealTypeEnum.Dinner
+  ]
+  const difficultyArr = [
+    DifficultyEnum.Easy,
+    DifficultyEnum.Medium,
+    DifficultyEnum.Hard
+  ]
+  const costArr = [CostEnum.Budget, CostEnum.Expensive, CostEnum.Moderate]
+
   const title = faker.lorem.words(2)
-  const ingredients = faker.lorem.paragraph(2)
-  const method = faker.lorem.paragraph(3)
+  let ingredients = []
+  for (let index = 0; index < 5; index++)
+    ingredients.push(faker.lorem.sentence())
+  let method = []
+  for (let index = 0; index < 5; index++) method.push(faker.lorem.sentence())
+  const randomMealType: MealTypeEnum = faker.random.arrayElement(mealTypeArr)
+  const randomDifficulty: DifficultyEnum = faker.random.arrayElement(
+    difficultyArr
+  )
+  const randomCost: CostEnum = faker.random.arrayElement(costArr)
 
   const recipe = new Recipe()
   recipe.title = title
@@ -28,12 +40,11 @@ define(Recipe, (
     '#spaghetti',
     '#nomeatmay'
   ]
-  recipe.lowResolution = 'test url for low res'
-  recipe.standardResolution = 'test url for standard res'
-  recipe.mealType = settings.mealType
-  recipe.difficulty = settings.difficulty
-  recipe.cost = settings.cost
-  recipe.recipeAttribution = settings.recipeAttribution
+  recipe.lowResolution = faker.image.food(320, 320)
+  recipe.standardResolution = faker.image.food(640, 640)
+  recipe.mealType = randomMealType
+  recipe.difficulty = randomDifficulty
+  recipe.cost = randomCost
 
   return recipe
 })
--- src/db/seeds/003_recipe.seed.ts
+++ src/db/seeds/003_recipe.seed.ts
@@ -1,20 +1,25 @@
-import { Seeder } from 'typeorm-seeding'
+import { Seeder, times, Factory } from 'typeorm-seeding'
 import { Connection } from 'typeorm'
 
 import Recipe from '../entities/Recipe'
 import RecipeAttribution from '../entities/RecipeAttribution'
-import { MealTypeEnum, DifficultyEnum, CostEnum } from '../entities/Recipe'
 
 export default class CreateRecipes implements Seeder {
-  public async run(factory: any, connection: Connection): Promise<any> {
-    const recipeAttribution = await connection
-      .getRepository(RecipeAttribution)
-      .findOne()
-    await factory(Recipe)({
-      mealType: MealTypeEnum.Breakfast,
-      difficulty: DifficultyEnum.Easy,
-      cost: CostEnum.Budget,
-      recipeAttribution
-    }).seedMany(3)
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    const db = connection.createEntityManager()
+    let count = 0
+    const recipeAttributionEven = await factory(RecipeAttribution)().seed()
+    await times(15, async () => {
+      count++
+      // Creates a recipeAttribution in the db
+      const recipeAttribution = await factory(RecipeAttribution)().seed()
+      // This only returns a entity with fake data
+      // with same recipeAttribution or different
+      const recipe = await factory(Recipe)().make()
+      if (count % 2) recipe.recipeAttribution = recipeAttributionEven
+      else recipe.recipeAttribution = recipeAttribution
+
+      await db.save(recipe)
+    })
   }
 }
