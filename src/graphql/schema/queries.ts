c59f20e6280f25c369628f66358ee4fbb9843a0e
John Stewart <johncodeinaire@gmail.com>
2020-02-09 19:57:38 +1100

    Remove - temporarily remove article mutations and query (#21) (#22)

--- src/graphql/schema/queries.ts
+++ src/graphql/schema/queries.ts
@@ -2,10 +2,11 @@ const { gql } = require('apollo-server-lambda')
 
 export default gql`
   type Query {
+    # TODO - reinstate if we get the articles function working
+    # articles: [Article]
     challenge(recipeId: ID!): Challenge
     recipes: [Recipe]!
     recipe(recipeId: ID, recipeTitle: String): Recipe!
-    articles: [Article]
     me(id: String!): UserProfile
   }
 `
